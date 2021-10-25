0904fc3cdf861973f189a46b053d5dfff436a04d
marekskibinski <marekskibinski@gmail.com>
2021-10-25 17:03:30 +0100

    Added new welcome

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,6 +129,10 @@ const controlAddRecipe = async function (newRecipe) {
   //  console.log(newRecipe);
 };
 
+const newFeature = function () {
+  console.log('Welcome to the app');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -137,6 +141,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
